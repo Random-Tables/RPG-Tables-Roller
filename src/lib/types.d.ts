f4fb13128d8778317d33a6b3e6f4e0a03ff3e59c
aronedwards91 <aronedwards91@protonmail.com>
2021-06-24 22:03:07 +0100

    example data & types

--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -5,3 +5,16 @@
 export interface Locals {
 	userid: string;
 }
+export type tableIndex = {
+	collectionID: string, // used as address
+	collectionName: string,  // Readable Name
+	category: string, // preferred category
+	isUtility: boolean, // If true not shown, but used in background
+	tags: string[],
+	tables: string[], // list of tableIds 
+}
+export type tableItem = {
+	tableID: string,
+	tableSections: object,
+}
+export type tableData = tableItem[];
