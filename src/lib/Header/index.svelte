3a5121bc3f7489db656eacccd672ac92e4dafcfe
aronedwards91 <aronedwards91@protonmail.com>
2021-08-25 15:45:59 +0100

    settings page

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,57 +1,22 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from './svelte-logo.svg';
 </script>
 
 <header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
 		<ul>
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.path === '/table'}><a sveltekit:prefetch href="/table">Tables</a></li>
-			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
+			<li class:active={$page.path === '/settings'}><a sveltekit:prefetch href="/settings">Settings</a></li>
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
 	</nav>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
 </header>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
 	}
 
 	nav {
---
+++ src/routes/settings.svelte
@@ -0,0 +1,58 @@
+<script>
+	import { types, settings, settingsStatus } from '$lib/settings';
+
+	let settingsStatuslocal;
+	settingsStatus.subscribe((value) => {
+		settingsStatuslocal = value;
+	});
+	function onUpdateInput(key, newValue) {
+		const newObj = Object.assign(settingsStatuslocal, {});
+		newObj[key] = newValue;
+		console.log('newObj', newObj);
+		settingsStatus.update(() => newObj);
+	}
+	console.log('settingsStatuslocal', settingsStatuslocal);
+</script>
+
+<div>
+	<h3>Settings</h3>
+	<div class="settings">
+		{#each Object.keys(settings) as key}
+			<div class="setting-item">
+				<label>{settings[key].text}</label>
+				{#if settings[key].type === types.bool}
+					<input
+						type="checkbox"
+						bind:checked={settingsStatuslocal[key]}
+						on:change={(evt) => onUpdateInput(key, evt.currentTarget.checked)}
+					/>
+				{/if}
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.settings {
+		padding-left: 2rem;
+		margin-left: 2rem;
+		border-left: 1px solid grey;
+	}
+	.setting-item {
+		padding: 5px 0;
+		display: flex;
+		align-items: center;
+	}
+	.setting-item label {
+		min-width: 60vw;
+		display: inline-block;
+		font-size: 18px;
+	}
+	.setting-item input {
+		width: 1.5rem;
+		height: 1.5rem;
+		border-radius: 0.25em;
+		border: 0.1em solid currentColor;
+		background: transparent;
+	}
+</style>
